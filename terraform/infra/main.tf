3a88bf1a0d716cab5038c71e2a71c99d52cdae14
unknown <akankshakumbhare27@gmail.com>
2026-05-20 17:16:43 +0530

    Configure nip.io ingress

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -104,3 +104,35 @@ resource "helm_release" "cert_manager" {
     azurerm_kubernetes_cluster.aks
   ]
 }
+
+resource "kubernetes_manifest" "letsencrypt_prod" {
+  depends_on = [helm_release.cert_manager]
+
+  manifest = {
+    apiVersion = "cert-manager.io/v1"
+    kind       = "ClusterIssuer"
+
+    metadata = {
+      name = "letsencrypt-prod"
+    }
+
+    spec = {
+      acme = {
+        email  = "akankshakumbhare27@gmail.com"
+        server = "https://acme-v02.api.letsencrypt.org/directory"
+
+        privateKeySecretRef = {
+          name = "letsencrypt-prod"
+        }
+
+        solvers = [{
+          http01 = {
+            ingress = {
+              class = "nginx"
+            }
+          }
+        }]
+      }
+    }
+  }
+}
