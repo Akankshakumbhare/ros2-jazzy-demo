09ee1f20053d42894c268e00d2402fdc09e1bdc4
unknown <akankshakumbhare27@gmail.com>
2026-05-21 11:50:01 +0530

    added ingress controler verification

--- terraform/infra/ingress.tf
+++ terraform/infra/ingress.tf
@@ -2,12 +2,18 @@ resource "helm_release" "nginx_ingress" {
   name             = "ingress-nginx"
   namespace        = "ingress-nginx"
   create_namespace = true
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-internal"
+    value = "false"
+  }
 
   depends_on = [
     azurerm_kubernetes_cluster.aks,
     kubernetes_namespace.production
   ]
 
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
+  
 }
